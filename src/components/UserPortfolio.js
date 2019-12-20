2828a75020bc7eb00e10b5fb1f53755883e86c1e
Josh Turan <joshturan@Joshs-MacBook-Air.local>
2019-12-20 20:49:29 +0100

    Added uploadsService component

--- src/components/UserPortfolio.js
+++ src/components/UserPortfolio.js
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import uploadsService from '../services/uploads'
 
 const UserPortfolio = () => {
   const [uploads, setUploads] = useState([])
 
   const username = JSON.parse(window.localStorage.getItem('loggedTFPappUser')).username
 
-  const getUploads = () => {
-    const request = axios.get(`http://localhost:3004/${username}`, { params: { username: username } })
-    return request.then(response => response.data)
-  }
-
   useEffect(() => {
-    getUploads().then(allUploads => setUploads(allUploads))
+    uploadsService.getUploads().then(allUploads => setUploads(allUploads))
   }, [])
   console.log('uploads', uploads)
 
   const usersPortfolio = uploads.map(upload => {
-    return <img key={upload}
+    return <img key={upload} className="userPortfolio"
       src={require(`/Users/joshturan/tfp-frontend/public/uploads/${username}/${upload}`)}
       alt=""
       height="200px"
       width="200px"
     />
   })
-
-
 
   return (
     <div>
---
+++ src/services/uploads.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+
+const username = JSON.parse(window.localStorage.getItem('loggedTFPappUser')).username
+
+const getUploads = () => {
+  const request = axios.get(`http://localhost:3004/${username}`, { params: { username: username } })
+  return request.then(response => response.data)
+}
+
+export default { getUploads }
