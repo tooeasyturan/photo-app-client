e4ddee91ca5186bc37f999d44049daeb01b184ea
Josh Turan <joshturan@Joshs-Air.lan>
2020-05-10 19:07:06 +0200

    update

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,37 +1,21 @@
 import React, { useContext } from 'react'
 import { Grid } from 'semantic-ui-react'
 import PortfolioUploads from './PortfolioUploads'
-import EditModel from './EditModel'
-import EditPhotographer from './EditPhotographer'
-import FetchPortfolioUploads from './FeatchPortfolioUploads'
-
 import CreateProfile from './CreateProfile'
 import EditProfile from './EditProfile'
-
 import { UserContext } from './UserContext';
-
-
-// Parent is app component
 
 const MyProfile = () => {
   const [user, setUser] = useContext(UserContext)
   const { profile } = user
 
-  console.log('MY PROFILE', user.profile.length)
-
   return (
-    <>
-      <Grid>
-        <Grid.Column>
-          {/* {user.status === 'model' ? <EditModel user={user} loggedInUser={user} /> :
-            <EditPhotographer user={user} loggedInUser={user} />} */}
-          {profile.length === 0 ? <CreateProfile /> : <EditProfile />}
-          {/* <DeleteUser /> */}
-          <PortfolioUploads />
-          {/* <FetchPortfolioUploads user={user} loggedInUser={loggedInUser} /> */}
-        </Grid.Column>
-      </Grid>
-    </>
+    <Grid>
+      <Grid.Column>
+        {profile.length === 0 ? <CreateProfile /> : <EditProfile />}
+        <PortfolioUploads />
+      </Grid.Column>
+    </Grid>
   )
 }
 
