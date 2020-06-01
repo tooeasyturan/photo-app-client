f0465d263c2441cd81fd29ae3f42240e270ac635
Josh Turan <joshturan@Joshs-Air.lan>
2020-06-01 17:16:59 +0200

    updates messages service to tsx

--- src/services/messages.tsx
+++ src/services/messages.tsx
@@ -3,21 +3,51 @@
 import { apiRequestWithToken } from "./apiRequest";
 const messagesUrl = "messages";
 
-const createMessage = async (newMessage: {}) => {
+interface NewMessage {
+  userFrom: string;
+  userTo: string;
+  message: string;
+}
+
+interface Conversation {
+  convoId: string;
+  id: string;
+  message: {
+    content: string;
+    date: string;
+    sender: string;
+    _id: string;
+  };
+}
+
+interface AllMessages {
+  deleteByReceiver: string | null;
+  deletebySender: string | null;
+  id: string;
+  members: string[];
+  receiver: string;
+  sendeer: string;
+}
+
+const createMessage = async (newMessage: NewMessage) => {
   const res = await apiRequestWithToken(messagesUrl, "post", newMessage);
   return res.data;
 };
 
-const getAllMessages = async () => {
+const getAllMessages = async (): Promise<AllMessages> => {
   const res = await apiRequestWithToken(messagesUrl, "get");
+  console.log("get all messages", res.data);
   return res.data;
 };
 
-const getConversation = async (conversationId: string) => {
+const getConversation = async (
+  conversationId: string
+): Promise<Conversation> => {
   const res = await apiRequestWithToken(
     `${messagesUrl}/${conversationId}`,
     "get"
   );
+  console.log("get conversation", res.data);
   return res.data;
 };
 
