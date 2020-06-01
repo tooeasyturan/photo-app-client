9b0720702a68ae8030ef18ef123d31117dbb0069
Josh Turan <joshturan@Joshs-MacBook-Air.local>
2020-06-01 13:32:36 +0200

    added PrivateRoute to routes file

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -10,13 +10,9 @@ import MyProfile from "../my-profile/MyProfile";
 import GetAllMessages from "../messaging/GetAllMessages";
 import Profile from "../profiles/Profile";
 import Profiles from "../profiles/Profiles.tsx";
-import { PrivateRoute } from "../PrivateRoute";
+import PrivateRoute from "../PrivateRoute";
 
 const Routes = () => {
-  // const [loggedInUser, setLoggedInUser] = useState(
-  //   JSON.parse(window.localStorage.getItem("loggedInUser")) || ""
-  // );
-
   const loggedInUser = JSON.parse(window.localStorage.getItem("loggedInUser"));
   const username = loggedInUser ? loggedInUser.username : "";
 
@@ -26,10 +22,10 @@ const Routes = () => {
       <Route exact path='/' component={Landing} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
-      <Route exact path={`/${username}`} component={MyProfile} />
+      <PrivateRoute exact path={`/${username}`} component={MyProfile} />
       <Route exact path='/profiles' component={Profiles} />
       <Route exact path='/profiles/:username' component={Profile} />
-      <Route exact path='/inbox' component={GetAllMessages} />
+      <PrivateRoute exact path='/inbox' component={GetAllMessages} />
     </>
   );
 };
